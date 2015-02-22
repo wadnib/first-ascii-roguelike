d405ecdda0f17f5e368aef24cff6414283898abb
wadnib <wadnib@gmail.com>
2015-02-22 19:56:07 +0000

    Draw the map.

--- init/rl.js
+++ init/rl.js
@@ -16,6 +16,29 @@ var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
 function create() {
 	// init keyboard commands.
 	game.input.keyboard.addCallbacks(null, null, onKeyUp);
+	
+	// init map.
+	initMap();
+	
+	// init screen.
+	asciidisplay = [];
+	
+	for (var y = 0; y < ROWS; y++) {
+		var newRow = [];
+		asciidisplay.push(newRow);
+		
+		for (var x = 0; x < COLS; x++) {
+			newRow.push(initCell('', x, y));
+		}
+	}
+	
+	drawMap();
+}
+
+function initCell(chr, x, y) {
+	// add a single cell in a given position to the ascii display.
+	var style = {font: FONT + "px monospace", fill: "#fff"};
+	return game.add.text(FONT * 0.6 * x, FONT * y, chr, style);
 }
 
 function onKeyUp(event) {
@@ -37,7 +60,7 @@ var map;
 function initMap() {
 	// create a new random map.
 	map = [];
-	for (var y = 0; x < ROWS; y++) {
+	for (var y = 0; y < ROWS; y++) {
 		var newRow = [];
 		for (var x = 0; x < COLS; x++) {
 			if (Math.random() > 0.8)
@@ -47,4 +70,13 @@ function initMap() {
 		}
 		map.push(newRow);
 	}
+}
+
+// the ascii display, as a 2d array of characters.
+var asciidisplay;
+
+function drawMap() {
+	for (var y = 0; y < ROWS; y++)
+		for (var x = 0; x < COLS; x++)
+			asciidisplay[y][x].content = map[y][x];
 }
