f23e3dae044f03e4185600efd79ddb9d7fb27d01
wadnib <wadnib@gmail.com>
2015-02-22 20:16:55 +0000

    Init and Draw Actors

--- init/rl.js
+++ init/rl.js
@@ -20,6 +20,9 @@ function create() {
 	// init map.
 	initMap();
 	
+	// init actors.
+	initActors();
+	
 	// init screen.
 	asciidisplay = [];
 	
@@ -33,6 +36,7 @@ function create() {
 	}
 	
 	drawMap();
+	drawActors();
 }
 
 function initCell(chr, x, y) {
@@ -80,3 +84,50 @@ function drawMap() {
 		for (var x = 0; x < COLS; x++)
 			asciidisplay[y][x].content = map[y][x];
 }
+
+// a list of all the actors; 0 is the player.
+var player;
+var actorList;
+var livingEnimies;
+
+// points to each actor in it's position, for quick searching.
+var actorMap;
+
+function randomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function initActors() {
+	// create actors at random locations.
+	actorList = [];
+	actorMap = [];
+	
+	for (var e = 0; e < ACTORS; e++) {
+		// create new actor.
+		var actor = {x:0, y:0, hp:e == 0?3:1};
+		do {
+			// pick a random position that is both a floor and not occupied.
+			actor.y = randomInt(ROWS);
+			actor.x = randomInt(COLS);
+		} while (map[actor.y][actor.x] == '#' || actorMap[actor.y + "_" + actor.x] != null);
+		
+		// add references to the actor to the actor's list and map.
+		actorMap[actor.y + "_" + actor.x] = actor;
+		actorList.push(actor);
+	}
+	
+	// the player is the first actor in the list.
+	player = actorList[0];
+	livingEnimies = ACTORS - 1;
+}
+
+function drawActors() {
+	for (var a in actorList) {
+		if (actorList[a].hp > 0)
+			asciidisplay[actorList[a].y][actorList[a].x].content = a == 0?''+player.hp:'e';
+	}
+}
+
+
+
+
